e41b11582ff8548cd53713ee6adeefd3f2402150
Cyrus Proctor <cproctor@tacc.utexas.edu>
2018-12-10 16:37:59 +0000

    Comment out yum update since lustre not currently available on 7.6.

--- deploy/init_master.sh
+++ deploy/init_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y update
+#yum -y update
 yum -y install centos-release-openstack-rocky
 yum -y install python2-openstackclient python2-openstacksdk
 yum -y install vim git emacs
--- deploy/init_vm.sh
+++ deploy/init_vm.sh
@@ -3,5 +3,5 @@
 # Remove the first several characters of root's authorized keys file to allow ssh entry
 sed -i 's:^.*\(ssh-rsa\):\1:' /root/.ssh/authorized_keys
 
-yum -y update
+#yum -y update
 yum -y install vim git emacs
