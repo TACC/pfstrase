ada0c1d8ee2b4d9ca83f2e956378f3d0200b0d85
Richard Evans <rtevans@tacc.utexas.edu>
2020-10-05 09:14:59 -0500

    Update build_and_deploy.sh

--- client/build_and_deploy.sh
+++ client/build_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make dist
-cp pfstrased-0.0.1.tar.gz ~/rpmbuild/SOURCES/
+cp pfstrased-1.0.0.tar.gz ~/rpmbuild/SOURCES/
 cp pfstrase.spec ~/rpmbuild/SPECS/
 
 rpmbuild -bb ~/rpmbuild/SPECS/pfstrase.spec
